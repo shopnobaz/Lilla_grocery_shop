269655ce306e8c5d6f3da07efdfdc77c67134818
shopnobaz <vision2025bd@yahoo.com>
2022-04-28 17:20:26 +0200

    fixed bdd login tets waiting time

--- cucumber-wdio/step-definitions/login.js
+++ cucumber-wdio/step-definitions/login.js
@@ -1,5 +1,5 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
-const pauseTime = 10000000;
+const pauseTime = 3000;
 
 // browser.url - navigate to a page/url
 // browser.pause - pause execution for a number of ms
@@ -8,18 +8,18 @@ const pauseTime = 10000000;
 
 Given('that I can see the login button', async () => {
     await browser.url('/');
-    browser.pause(pauseTime)
+    await browser.pause(pauseTime)
 });
 
 When('I click the login button at the navbar', async () => {
     let grab_login_button = await $('.login');
     await grab_login_button.click();
-    browser.pause(pauseTime)
+    await browser.pause(pauseTime)
 });
 When('I should be seen Login form', async () => {
     let login_form = await $('.loginModal');
     await login_form.waitForDisplayed(pauseTime);
-    browser.pause(pauseTime)
+    await browser.pause(pauseTime)
 });
 
 When(/^I fill in the form with "(.*)" and "(.*)"$/, async (email, password) => {
@@ -29,19 +29,19 @@ When(/^I fill in the form with "(.*)" and "(.*)"$/, async (email, password) => {
     await email_login_box.waitForDisplayed(pauseTime);
     await password_login_box.setValue(password);
     await password_login_box.waitForDisplayed(pauseTime);
-    browser.pause(pauseTime)
+    await browser.pause(pauseTime)
 });
 When('I click the login button in the form', async () => {
     let login_button = await $('form[name="login"] button[name="login"]');
     await login_button.waitForDisplayed(pauseTime);
     await login_button.click();
-    browser.pause(pauseTime)
+    await browser.pause(pauseTime)
     await browser.url('/');
 });
 
 Then('I should be logged in and see logout button', async () => {
     let logout_button = await $('.navButtons .logout');
     await logout_button.waitForDisplayed(pauseTime);
-    browser.pause(pauseTime)
+    await browser.pause(pauseTime)
     await browser.url('/');
 });
