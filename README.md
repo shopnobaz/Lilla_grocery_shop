3210f2dc9f75c7948c0ff3fb97bc454f0e76e14b
shopnobaz <vision2025bd@yahoo.com>
2022-04-29 12:18:16 +0200

    Revert "Merge branch 'test' into web-driver-io-test"
    This reverts commit c57e24b84d314ed91a4c65619e1f1e675ef22e72, reversing
    changes made to ce6494b20de867fdd9534a91514229405e994e3f.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+# webdriver-io-github-actions
+Run WebDriverIO tests using GitHub Actions
+
+this is from web-driver-io test branch (just testing)
+commit from jahed on  web-driver-io-test
+
+This is a contribution test
+new contributor
+commit from jahed
 
--- test/test-Product.test.js
+++ test/test-Product.test.js
@@ -1,19 +1,6 @@
 const { expect } = require('@jest/globals');
 
-require('./fakeDOM.js');
-
-const { listen } = require('../frontend/helpers');
-
-global.listen = listen; // make listen available for all other script files
-
-
-
 const Product = require('../frontend/Product.js');
-
-
-const { render, renderInList, addEventListeners } = require('../frontend/Product.js');
-
-
 
 describe('Test the Product class', () => {
 
@@ -29,28 +16,6 @@ describe('Test the Product class', () => {
 
   });
 
-
-  test('Creation of a New  Product', () => {
-
-    let myProduct = new Product(101, 'Oliv oil ', 53, `Our organic extra virgin olive oil is made from olives from specially selected Italian plantations. 
-    Fruity and smooth oil with hints of herbs.
-    The term extra virgin olive oil is a term used for olive oil to ensure that the olive oil is made by cold pressing and that no chemical substances have been used during or after production.
-    Extra virgin olive oil is the same as cold - pressed olive oil.
-    The guarantor's eco - label states that a product is organic and sometimes also KRAV - labeled.Our range is constantly being replenished with more EKO products.`);
-
-    // Check that the constructor really sets the correct property values and myproduct propery matches with product from database product.
-    expect(myProduct.id).toBe(101);
-    expect(myProduct.name).toBe('Oliv oil ');
-    expect(myProduct.price).toBe(53);
-    expect(myProduct.description).toBe(`Our organic extra virgin olive oil is made from olives from specially selected Italian plantations. 
-    Fruity and smooth oil with hints of herbs.
-    The term extra virgin olive oil is a term used for olive oil to ensure that the olive oil is made by cold pressing and that no chemical substances have been used during or after production.
-    Extra virgin olive oil is the same as cold - pressed olive oil.
-    The guarantor's eco - label states that a product is organic and sometimes also KRAV - labeled.Our range is constantly being replenished with more EKO products.`);
-
-  });
-
-
   test('An id not equal to a number should fail for Product constructor', () => {
 
     expect(() => {
@@ -61,70 +26,8 @@ describe('Test the Product class', () => {
       new Product(true, 'X', 200, 'description');
     }).toThrow();
 
-  })
-
-  test('A name  not equal to a string  should fail for Product constructor', () => {
-
-    expect(() => {
-      new Product(1, 22222222, 200, 'description');
-    }).toThrow();
-
-    expect(() => {
-      new Product(2, true, 200, 'description');
-    }).toThrow();
 
   })
 
-
-  /*   test('should output id, image and name,price', () => {
-      //renderInList(myProduct); should output data less test 
-  
-      const testproduct = renderInList(104, "https://www.halegroves.com/images/xl/buy-navel-oranges-online-123020.jpg", 'Clementiner Klass 1', 32);
-      expect(testproduct).toBe(104, "https://www.halegroves.com/images/xl/buy-navel-oranges-online-123020.jpg", 'Clementiner Klass 1', 32);
-  
-    });
-  
-  
-    test('renderInList method should output data less text', () => {
-      //renderInList(Product); should output data less test 
-  
-      let testproduct = renderInList(null, '', '', null);
-      expect(testproduct).toBe(null, '', '', null);
-  
-    }); */
-
-
-});
-
-
-describe('Checking the products propertise', () => {
-
-  test('Name is Oliv oil', () => {
-    const name = 'Oliv oil';
-    expect(name).toBe('Oliv oil');
-  });
-
-  test('Id is 101', () => {
-    const id = 101;
-    expect(id).toBe(101);
-  });
-
-  test('Name is Oliv oil', () => {
-    const description = `Our organic extra virgin olive oil is made from olives from specially selected Italian plantations. 
-    Fruity and smooth oil with hints of herbs.
-    The term extra virgin olive oil is a term used for olive oil to ensure that the olive oil is made by cold pressing and that no chemical substances have been used during or after production.
-    Extra virgin olive oil is the same as cold - pressed olive oil.
-    The guarantor's eco - label states that a product is organic and sometimes also KRAV - labeled.Our range is constantly being replenished with more EKO products.`;
-    expect(description).toBe(`Our organic extra virgin olive oil is made from olives from specially selected Italian plantations. 
-    Fruity and smooth oil with hints of herbs.
-    The term extra virgin olive oil is a term used for olive oil to ensure that the olive oil is made by cold pressing and that no chemical substances have been used during or after production.
-    Extra virgin olive oil is the same as cold - pressed olive oil.
-    The guarantor's eco - label states that a product is organic and sometimes also KRAV - labeled.Our range is constantly being replenished with more EKO products.`);
-  });
-
-  test('Price is 53', () => {
-    const price = 53;
-    expect(price).toBe(53);
-  });
 
 });
