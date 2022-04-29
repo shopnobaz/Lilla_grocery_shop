64dd68d5ad0c229f1fb1fcef3d260a2b94cd51c8
shopnobaz <vision2025bd@yahoo.com>
2022-04-29 09:53:31 +0200

    test folder, db

---
+++ test/test-grabEl.test.js
@@ -0,0 +1,24 @@
+require('./fakeDOM.js');
+
+const { grabEl, grabEls } = require('../frontend/helpers');
+
+describe('Test grabEl', () => {
+
+  test('grabEL function exists', () => {
+    expect(grabEl).toBeDefined();
+  });
+
+  test('Get the content of header tag is correct', () => {
+
+    let content = grabEl('header').innerHTML.trim();
+
+    expect(content.replaceAll(' ', '')).toBe(`<h1>Our grocery shop</h1>
+    <div class="navButtons">
+  <button class="showCart">Show cart</button>
+  <button class="login">Login</button>
+  <button class="register">Register</button>
+  <button class="logout">Logout</button>
+    </div>`.replaceAll(' ', ''));
+  });
+
+})
