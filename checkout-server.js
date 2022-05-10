0969c8ec63092048f891245eb356f02cd81d6d7c
shopnobaz <vision2025bd@yahoo.com>
2022-05-10 22:09:34 +0200

    added to checkout-server.js

--- checkout-server.js
+++ checkout-server.js
@@ -3,9 +3,33 @@ const http = require('http');
 
 // Use the built in execSync commando that can run 
 // command line/bash commands
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
 
 const path = require('path');
+
+
+//Read the deployment key from the environment variable
+const secret = process.env.DEPLOYMENT_KEY;
+
+//If there is not secret key-shutdown
+if (!secret) {
+  console.log('You need provide the DEPLOYMENT_KEY as en environment variable');
+  process.exit(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Path to db-template
 const dbTemplatePath = path.join(__dirname, 'database', 'products-template.db');
@@ -14,14 +38,14 @@ const dbPath = path.join(__dirname, 'database', 'products.db');
 
 
 //A  funcction that does all necessary git checkout, cleanup etc
-function checkout(){
-    execSync('git pull');
-    execSync('npm install'); // install new npm modules mentioned in package.json
-    execSync('rm ' + dbPath); // remove the database
-    execSync('cp ' + dbTemplatePath + ' ' + dbPath); // copy dbTemplate to db
-    //execSync('npm run build'); // build the dist folder that will be served
-    execSync('pm2 restart dev-main'); // restart our main app
-    console.log('Pulled, copied db and restarted the server');
+function checkout() {
+  execSync('git pull');
+  execSync('npm install'); // install new npm modules mentioned in package.json
+  execSync('rm ' + dbPath); // remove the database
+  execSync('cp ' + dbTemplatePath + ' ' + dbPath); // copy dbTemplate to db
+  //execSync('npm run build'); // build the dist folder that will be served
+  execSync('pm2 restart dev-main'); // restart our main app
+  console.log('Pulled, copied db and restarted the server');
 }
 
 // Set up a small server that only check out things if know
@@ -35,5 +59,5 @@ const server = http.createServer(function (req, res) {
 });
 
 // Start up the server 
- 
+
 server.listen(9876, () => console.log('Listening on http://localhost:9876'));
